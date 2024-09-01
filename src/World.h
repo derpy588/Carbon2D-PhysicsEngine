410eba2ed4d4f72d01620c18a88d5e6611fcbf74
derpy588 <willcpeacock@gmail.com>
2024-08-31 23:41:54 -0400

    Basic world class

---
+++ src/World.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Vec.h"
+#include "Object.h"
+#include <vector>
+
+class World
+{
+private:
+    Vec2 m_gravity {0, -9.81};
+    std::vector<Object*> m_objects;
+
+public:
+    void AddObject(Object* object);
+    void RemoveObject(Object* object);
+
+    void Step(float dt);
+
+};
