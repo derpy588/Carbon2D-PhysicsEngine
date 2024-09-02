adae7e0d3355c29b4b476a5f87b270db5eeaecb9
derpy588 <willcpeacock@gmail.com>
2024-09-01 23:11:07 -0400

    Implemented Vec2 and Vec3 operators

--- src/Vec.h
+++ src/Vec.h
@@ -6,29 +6,47 @@ struct Vec2
     float y {0};
 };
 
-Vec2 operator*(Vec2 a, Vec2 b) {
-    return {(a.x * b.x), (a.y * b.y)};
-}
+// Vector-Vector Operators
+Vec2 operator+(Vec2 a, Vec2 b) { return {(a.x + b.x), (a.y + b.y)}; }
+Vec2 operator-(Vec2 a, Vec2 b) { return {(a.x - b.x), (a.y - b.y)}; }
+Vec2 operator*(Vec2 a, Vec2 b) { return {(a.x * b.x), (a.y * b.y)}; }
+Vec2 operator/(Vec2 a, Vec2 b) { return {(a.x / b.x), (a.y / b.y)}; }
 
-
+// scalar-vector operators
 template <typename T>
-Vec2 operator*(T a, Vec2 b) {
-    return {(a * b.x), (a * b.y)};
-}
-
+Vec2 operator+(T a, Vec2 b) { return {(a + b.x), (a + b.y)}; }
 template <typename T>
-Vec2 operator*(Vec2 a, T b) {
-    return {(a.x * b), (a.y * b)};
-}
-
+Vec2 operator-(T a, Vec2 b) { return {(a - b.x), (a - b.y)}; }
 template <typename T>
-Vec2 operator/(Vec2 a, T b) {
-    return {(a.x / b), (a.y / b)};
-}
+Vec2 operator*(T a, Vec2 b) { return {(a * b.x), (a * b.y)}; }
+template <typename T>
+Vec2 operator/(T a, Vec2 b) { return {(a / b.x), (a / b.y)}; }
 
-Vec2 operator+=(Vec2 a, Vec2 b) {
-    return {(a.x + b.x), (a.y + b.y)};
-}
+//Vector-scalar operators
+template <typename T>
+Vec2 operator+(Vec2 a, T b) { return {(a.x + b), (a.y + b)}; }
+template <typename T>
+Vec2 operator-(Vec2 a, T b) { return {(a.x - b), (a.y - b)}; }
+template <typename T>
+Vec2 operator*(Vec2 a, T b) { return {(a.x * b), (a.y * b)}; }
+template <typename T>
+Vec2 operator/(Vec2 a, T b) { return {(a.x / b), (a.y / b)}; }
+
+// Vector-Vector assignment operators
+Vec2 operator+=(Vec2 a, Vec2 b) { return {(a.x + b.x), (a.y + b.y)}; }
+Vec2 operator-=(Vec2 a, Vec2 b) { return {(a.x - b.x), (a.y - b.y)}; }
+Vec2 operator*=(Vec2 a, Vec2 b) { return {(a.x * b.x), (a.y * b.y)}; }
+Vec2 operator/=(Vec2 a, Vec2 b) { return {(a.x / b.x), (a.y / b.y)}; }
+
+// Vector-Scalar assignment operators
+template <typename T>
+Vec2 operator+=(Vec2& a, T b) { return a = a + b };
+template <typename T>
+Vec2 operator-=(Vec2& a, T b) { return a = a - b };
+template <typename T>
+Vec2 operator*=(Vec2& a, T b) { return a = a * b };
+template <typename T>
+Vec2 operator/=(Vec2& a, T b) { return a = a / b };
 
 struct Vec3
 {
@@ -36,3 +54,45 @@ struct Vec3
     float y {0};
     float z {0};
 };
+
+// Vector-Vector Operators
+Vec3 operator+(Vec3 a, Vec3 b) { return {(a.x + b.x), (a.y + b.y), (a.z + b.z)}; }
+Vec3 operator-(Vec3 a, Vec3 b) { return {(a.x - b.x), (a.y - b.y), (a.z - b.z)}; }
+Vec3 operator*(Vec3 a, Vec3 b) { return {(a.x * b.x), (a.y * b.y), (a.z * b.z)}; }
+Vec3 operator/(Vec3 a, Vec3 b) { return {(a.x / b.x), (a.y / b.y), (a.z / b.z)}; }
+
+// scalar-vector operators
+template <typename T>
+Vec3 operator+(T a, Vec3 b) { return {(a + b.x), (a + b.y), (a + b.z)}; }
+template <typename T>
+Vec3 operator-(T a, Vec3 b) { return {(a - b.x), (a - b.y), (a - b.z)}; }
+template <typename T>
+Vec3 operator*(T a, Vec3 b) { return {(a * b.x), (a * b.y), (a * b.z)}; }
+template <typename T>
+Vec3 operator/(T a, Vec3 b) { return {(a / b.x), (a / b.y), (a / b.z)}; }
+
+//Vector-scalar operators
+template <typename T>
+Vec3 operator+(Vec3 a, T b) { return {(a.x + b), (a.y + b), (a.z + b)}; }
+template <typename T>
+Vec3 operator-(Vec3 a, T b) { return {(a.x - b), (a.y - b), (a.z - b)}; }
+template <typename T>
+Vec3 operator*(Vec3 a, T b) { return {(a.x * b), (a.y * b), (a.z * b)}; }
+template <typename T>
+Vec3 operator/(Vec3 a, T b) { return {(a.x / b), (a.y / b), (a.z / b)}; }
+
+// Vector-Vector assignment operators
+Vec3 operator+=(Vec3 a, Vec3 b) { return {(a.x + b.x), (a.y + b.y), (a.z + b.z)}; }
+Vec3 operator-=(Vec3 a, Vec3 b) { return {(a.x - b.x), (a.y - b.y), (a.z - b.z)}; }
+Vec3 operator*=(Vec3 a, Vec3 b) { return {(a.x * b.x), (a.y * b.y), (a.z * b.z)}; }
+Vec3 operator/=(Vec3 a, Vec3 b) { return {(a.x / b.x), (a.y / b.y), (a.z / b.z)}; }
+
+// Vector-Scalar assignment operators
+template <typename T>
+Vec3 operator+=(Vec3& a, T b) { return a = a + b };
+template <typename T>
+Vec3 operator-=(Vec3& a, T b) { return a = a - b };
+template <typename T>
+Vec3 operator*=(Vec3& a, T b) { return a = a * b };
+template <typename T>
+Vec3 operator/=(Vec3& a, T b) { return a = a / b };
